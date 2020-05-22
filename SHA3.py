6a33c3044ff1af3caaa580910b4d82ace9fc4315
pkdoshinji <43355603+pkdoshinji@users.noreply.github.com>
2020-05-22 13:04:45 -0400

    formatting changes

--- SHA3.py
+++ SHA3.py
@@ -4,11 +4,7 @@ import sys
 import numpy as np
 import argparse
 
-
-filename = 'xeyess'
-
-
-# Keccak constants
+# Keccak constants, hard-coded for the SHA3 family of hashes 
 l_list = [0,1,2,3,4,5,6]
 l = l_list[6]
 w = (2 ** l)
@@ -31,7 +27,6 @@ RCs = [0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
        0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
        0x8000000000008080, 0x0000000080000001, 0x8000000080008008]
 
-
 def get_bitstring(message):
     # Convert the message to a string of 1s and 0s
     bitstring = ''
@@ -42,12 +37,10 @@ def get_bitstring(message):
     bitstring += '01100000' # Add the SHA-3 suffix
     return bitstring
 
-
 def get_bitstring_from_file(filename):
     with open(filename, 'rb') as fh:
         bytetext = fh.read()
     return bytes_to_bitstring(bytetext)
-
 
 def bytes_to_bitstring(in_bytes):
     bitstring = ''
@@ -57,8 +50,6 @@ def bytes_to_bitstring(in_bytes):
         bitstring += byte
     bitstring += '01100000'
     return bitstring
-
-
 
 def string_to_array(string,w=64):
     # Convert a bitstring to (5x5xw) numpy array
@@ -70,8 +61,6 @@ def string_to_array(string,w=64):
                     state_array[y][x][z] = int(string[w*(5*x+y)+z])
     return state_array
 
-
-
 def hex_to_array(hexnum, w=64):
     # Convert a hexstring to a 1-dimensional numpy array
     bitstring = '{0:064b}'.format(hexnum)
@@ -79,12 +68,10 @@ def hex_to_array(hexnum, w=64):
     array = np.array([int(bitstring[i]) for i in range(w)])
     return array
 
-
 def pad(rate, message_length):
     # Pad the bitstring using pad10*1, as per the SHA-3 specifications
     j = (-(message_length+1))%rate
     return '0' * j + '1'
-
 
 def theta(array, w=64):
     # For each column, XOR the parity of two adjacent columns
@@ -100,7 +87,6 @@ def theta(array, w=64):
             array_prime[x][y] ^= D[x] # For each lane, XOR the value of D[x]
     return array_prime
 
-
 def rho(array, w=64):
     # Circular shift each lane by a precalculated amount (given by the shifts array)
     array_prime = array.copy()
@@ -108,7 +94,6 @@ def rho(array, w=64):
         for y in range(5):
             array_prime[x][y] = np.roll(array[x][y], shifts[x][y])
     return array_prime
-
 
 def pi(array, w=64):
     # 'Rotate' each slice according to a modular linear transformation
@@ -118,7 +103,6 @@ def pi(array, w=64):
             array_prime[x][y] = array[((x) + (3 * y)) % 5][x]
     return array_prime
 
-
 def chi(array, w=64):
     # Bitwise transfoirmation of each row according to a nonlinear function
     array_prime = np.zeros(array.shape, dtype=int)
@@ -126,7 +110,6 @@ def chi(array, w=64):
         for y in range(5):
             array_prime[x][y] = array[x][y] ^ ((array[(x + 1) % 5][y]^1) & (array[(x + 2) % 5][y]))
     return array_prime
-
 
 def iota(array, round_index, w=64):
     # XOR each lane with a precalculated round constant
@@ -136,16 +119,14 @@ def iota(array, round_index, w=64):
     array_prime[0][0] ^= RC
     return array_prime
 
-
 def keccak(state):
     # The keccak function defines one transformation round, SHA-3 has 24 in total
     for round_index in range(24):
         state = iota(chi(pi(rho(theta(state)))),round_index)
     return state
 
-
 def squeeze(array, bits):
-   # 'Squeezing' phase of the sponge construction yields the hash
+    # 'Squeezing' phase of the sponge construction yields the hash
     hash = ''
     for i in range(5):
         for j in range(5):
@@ -159,11 +140,9 @@ def squeeze(array, bits):
                 hash += '{0:02x}'.format(int(byte,2))
     return hash[:int(bits/4)]
 
-
 def usage():
     print(f"[***] Usage: {sys.argv[0]} -m <message string> -o <output-bits (224, 256, 384, or 512)>")
     exit()
-
 
 def main():
     # The main event
@@ -200,20 +179,22 @@ def main():
     # Pad the bitstring according to the pad10*1 function (see SHA3 specifications)
     padded = bitstring + pad(rate, len(bitstring)%rate)
 
+    # The sponge function absorbs <rate> bits per round, so (len(padded) // rate) rounds total 
     sponge_rounds = len(padded) // rate
 
+    # Initialize the state array 
     state = np.zeros(b, dtype=int).reshape(5,5,w)
 
+    # For each sponge round, absorb <rate> bits and process the state array with the keccak permutation
     for i in range(sponge_rounds):
         current_string = padded[(i*rate):(i*rate) + rate]
         array = string_to_array(current_string, w=64)
         state = np.bitwise_xor(state, array)
         state = keccak(state)
 
-    # The 'squeeze' phase outputs the hash value
+    # The 'squeeze' phase outputs the final hash value
     print(squeeze(state, outbits))
     exit()
-
 
 if __name__ == '__main__':
     main()
